3806ca95f182c9a6a215fede870017e0d1b71bbc
Paul Wilson <pwilson802@gmail.com>
2020-04-10 13:38:36 +1000

    changed column names

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
+import os.path
 app = Flask(__name__)
 
 today = datetime.now()
+yesterday = datetime.now() - timedelta(days=1)
 file_today = f'{today.day}-{today.month}-{today.year}.json'
-with open(file_today) as json_file:
-    data = json.loads(json_file.read())
+file_yesterday = f'{yesterday.day}-{yesterday.month}-{yesterday.year}.json'
+
+
+if os.path.isfile(file_today):
+    with open(file_today) as json_file:
+        data = json.loads(json_file.read())
+else:
+    with open(file_yesterday) as json_file:
+        data = json.loads(json_file.read())
 
 @app.route('/')
 def index():
