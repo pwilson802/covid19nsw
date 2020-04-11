1ff2daa3f7157d4b77a3945694aa9807c804b822
Paul Wilson <pwilson802@gmail.com>
2020-04-11 22:38:40 +1000

    graphs added responsive

--- app.py
+++ app.py
@@ -12,8 +12,11 @@ file_today_name = f'{today.day}-{today.month}-{today.year}.json'
 file_yesterday_name = f'{yesterday.day}-{yesterday.month}-{yesterday.year}.json'
 file_today = os.path.join(data_folder,file_today_name)
 file_yesterday = os.path.join(data_folder,file_yesterday_name)
+postcode_file_today = os.path.join(data_folder,f'postcode-{file_today_name}')
+postcode_file_yesterday = os.path.join(data_folder,f'postcode-{file_yesterday_name}')
 
 
+# If todays file hasn't been loaded yet, we will grab yesterdays file
 if os.path.isfile(file_today):
     with open(file_today) as json_file:
         data = json.loads(json_file.read())
@@ -22,6 +25,15 @@ else:
         data = json.loads(json_file.read())
 
 all_postcodes = [x['postcode'] for x in data]
+
+# If todays postcode file hasn't been loaded yet, we will grab yesterdays file
+if os.path.isfile(postcode_file_today):
+    with open(postcode_file_today) as json_file:
+        postcode_history = json.loads(json_file.read())
+else:
+    with open(postcode_file_yesterday) as json_file:
+        postcode_history = json.loads(json_file.read())
+
 
 @app.route('/')
 def index():
@@ -35,6 +47,6 @@ def postcode():
         if postcode not in all_postcodes:
             return render_template('postcode_error.html', postcode = postcode)
         postcode_data = [x for x in data if x['postcode'] == postcode]
-        return render_template("postcode.html",data = postcode_data)
+        return render_template("postcode.html",data = postcode_data, history=postcode_history[postcode])
     else:
         return render_template('index.html', data=data)
