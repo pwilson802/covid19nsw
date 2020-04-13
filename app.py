33e16d56b17fc58da6c60001de2c3b303337843b
Paul Wilson <pwilson802@gmail.com>
2020-04-13 22:09:35 +1000

    final changes before push - day changes and search results

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import json
 from datetime import datetime, timedelta
 import os.path
+from difflib import get_close_matches
 app = Flask(__name__)
 
 data_folder = os.environ.get('COVID_DATA')
@@ -17,7 +18,7 @@ postcode_file_yesterday = os.path.join(data_folder,f'postcode-{file_yesterday_na
 seven_file_today = os.path.join(data_folder,f'seven-{file_today_name}')
 seven_file_yesterday = os.path.join(data_folder,f'seven-{file_yesterday_name}')
 fourteen_file_today = os.path.join(data_folder,f'fourteen-{file_today_name}')
-fourteen_file_yesterday = os.path.join(data_folder,f'fourteen-{file_yesterday}')
+fourteen_file_yesterday = os.path.join(data_folder,f'fourteen-{file_yesterday_name}')
 suburb_to_postcode_file = os.path.join(data_folder,'suburb_to_postcode.json')
 all_postcode_suburb_file = os.path.join(data_folder,'all_postcode_suburb.json')
 
@@ -62,6 +63,8 @@ with open(suburb_to_postcode_file) as json_file:
 with open(all_postcode_suburb_file) as json_file:
         all_postcode_suburb_list = json.loads(json_file.read())
 
+validation_list = [x.lower() for x in all_postcode_suburb_list]
+all_postcode_suburb_list = set(all_postcode_suburb_list)
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
@@ -69,39 +72,67 @@ def index():
         input_data = request.form
         days = input_data['days']
         if days == 'seven':
-            return render_template('index.html', data=data_seven_days, days_set='seven')
+            return render_template('index.html', data=data_seven_days, days_set='seven', validation_set = all_postcode_suburb_list)
         if days == 'fourteen':
-            return render_template('index.html', data=data_fourteen_days, days_set='fourteen')
-    return render_template('index.html', data=data, days_set = 'all' )
+            return render_template('index.html', data=data_fourteen_days, days_set='fourteen', validation_set = all_postcode_suburb_list)
+    return render_template('index.html', data=data, days_set = 'all', validation_set = all_postcode_suburb_list )
 
 @app.route('/postcode',methods = ['POST', 'GET'])
 def postcode():
+    print('-----------')
     if request.method == 'POST':
         input_data = request.form
         try:
             days = input_data['days']
-            print("------------------------")
-            print(days)
             if 'recall' in days:
                 _, postcode, days = days.split('-')
         except:
             days = 'all'
             postcode = input_data['postcode']
-        if postcode not in all_postcodes:
-            return render_template('postcode_error.html', postcode = postcode)
+        print(postcode)
+        if postcode.lower() not in validation_list:
+            close_options = get_close_matches(postcode.lower(), validation_list)
+            in_options = [x for x in validation_list if postcode in x]
+            all_options = [x.title() for x in set(in_options + close_options)]
+            if len(all_options) == 0:
+                other_options = 'no'
+            else:
+                other_options = 'yes'
+            return render_template('postcode_error.html', postcode = postcode.title(), other_options = other_options, all_options = all_options, validation_set = all_postcode_suburb_list)
+        # Need to find if the search is not a post code so we can convert it
+        if not postcode.startswith('2'):
+            postcode = suburb_to_postcode_dict[postcode.lower()]
         close_postcodes_try = [int(postcode) - 1, int(postcode) - 2, int(postcode) + 1, int(postcode) + 2]
         close_postcodes = [str(x) for x in close_postcodes_try if str(x) in all_postcodes]
         if days == 'seven':
             postcode_data = [x for x in data_seven_days if x['postcode'] == postcode]
             close_postcode_data = [x for x in data_seven_days if x['postcode'] in close_postcodes]
-            return render_template("postcode.html",postcode = postcode, data = postcode_data, history=postcode_history[postcode],days_set = 'seven', close = close_postcode_data)
+            return render_template("postcode.html",postcode = postcode, data = postcode_data, history=postcode_history[postcode],days_set = 'seven', close = close_postcode_data, validation_set = all_postcode_suburb_list)
         elif days == 'fourteen':
             postcode_data = [x for x in data_fourteen_days if x['postcode'] == postcode]
             close_postcode_data = [x for x in data_fourteen_days if x['postcode'] in close_postcodes]
-            return render_template("postcode.html",postcode = postcode, data = postcode_data, history=postcode_history[postcode],days_set = 'fourteen', close = close_postcode_data )
+            return render_template("postcode.html",postcode = postcode, data = postcode_data, history=postcode_history[postcode],days_set = 'fourteen', close = close_postcode_data, validation_set = all_postcode_suburb_list )
         else:
             postcode_data = [x for x in data if x['postcode'] == postcode]
             close_postcode_data = [x for x in data if x['postcode'] in close_postcodes]
-            return render_template("postcode.html",postcode = postcode, data = postcode_data, history=postcode_history[postcode],days_set = 'all', close = close_postcode_data)
+            return render_template("postcode.html",postcode = postcode, data = postcode_data, history=postcode_history[postcode],days_set = 'all', close = close_postcode_data, validation_set = all_postcode_suburb_list)
     else:
-        return render_template('index.html', data=data)
+        print(request.args['location'])
+        # try:
+        postcode = request.args['location']
+        print(postcode)
+        if not postcode.startswith('2'):
+            postcode = suburb_to_postcode_dict[postcode.lower()]
+        close_postcodes_try = [int(postcode) - 1, int(postcode) - 2, int(postcode) + 1, int(postcode) + 2]
+        close_postcodes = [str(x) for x in close_postcodes_try if str(x) in all_postcodes]
+        postcode_data = [x for x in data if x['postcode'] == postcode]
+        close_postcode_data = [x for x in data if x['postcode'] in close_postcodes]
+        return render_template("postcode.html",postcode = postcode, data = postcode_data, history=postcode_history[postcode],days_set = 'all', close = close_postcode_data, validation_set = all_postcode_suburb_list)
+        # except:
+        #     return render_template('index.html', data=data, validation_set = all_postcode_suburb_list)
+
+# @app.route('/new_postcode',methods = ['POST', 'GET'])
+# def new_postcode():
+#     postcode = request.args['new_postcode']
+#     print(postcode)
+#     return render_template('index.html', data=data, validation_set = all_postcode_suburb_list)
