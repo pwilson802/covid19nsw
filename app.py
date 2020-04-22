8b3bf8da30b6cb1c5f828d67a15508003148f22e
Paul Wilson <pwilson802@gmail.com>
2020-04-22 16:20:20 +1000

    changed map file name to use os.path

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ def postcode():
                 map_data = all_map('fourteen_day_count', postcode_zoom=postcode,zoom=13)
                 map_data.save(temp_map_name)
                 remove_bootstrap3(temp_map_name)
-                just_the_file = temp_map_name.split('\\')[-1]
+                just_the_file = os.path.split(temp_map_name)[-1]
                 map_template = f"temp_maps/{just_the_file}"
                 return render_template('map_direct.html', days_set="fourteen", view_mode="map", temp_map_name=map_template, temp_map = 'true')
             postcode_data = [x for x in data_fourteen_days if x["postcode"] == postcode]
@@ -226,7 +226,7 @@ def postcode():
                 map_data = all_map('all_count', postcode_zoom=postcode,zoom=13)
                 map_data.save(temp_map_name)
                 remove_bootstrap3(temp_map_name)
-                just_the_file = temp_map_name.split('\\')[-1]
+                just_the_file = os.path.split(temp_map_name)[-1]
                 map_template = f"temp_maps/{just_the_file}"
                 return render_template('map_direct.html', days_set="all", view_mode="map", temp_map_name=map_template, temp_map = 'true')
             postcode_data = [x for x in data if x["postcode"] == postcode]
